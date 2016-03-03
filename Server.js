92d2cc300ff65805fd0653e1ddd9cbe147a58745
XuiKoh <silversui@hotmail.com>
2016-03-03 14:34:17 +1000

    update

--- Server.js
+++ Server.js
@@ -4,6 +4,9 @@ var async = require('async');
 var app = express();
 var server = http.Server(app);
 
+server.listen((process.env.PORT || 80), function(){
+  console.log('listening on *:80');
+});
 // Set Folder AS Root Folder
 app.use(express.static(__dirname + '/Images'));
 app.use(express.static(__dirname + '/Script'));
@@ -19,7 +22,4 @@ app.get('/Flickr/:parameterflickrtags', Flickr.FlickrApi);
 app.get('/Foursquare/:foursquareparameter',Flickr.FourSquareApi);
 app.get('/FoursquareID/:foursquareidparameter',Flickr.FourSquareApiByID);
 
-server.listen((process.env.PORT || 80), function(){
-  console.log('listening on *:80');
-});
-console.log('Listening on port 80');
+
